21ea9061a1937e1bb8f29e8b1574043740369868
Ajeet Pal <ajeetpal9193@gmail.com>
2022-03-23 08:40:14 +0530

    added Division too.

--- billing-master/src/main/java/com/arielmorel/billing/util/Multipy.java
+++ billing-master/src/main/java/com/arielmorel/billing/util/Multipy.java
@@ -5,4 +5,8 @@ public class Multiply{
   int b = 5;
   int c = a*b;
 	System.out.println("Multiplication of Two number is : " + c);
+
+	System.out.println("Add Division too in this.");
+	int d =b/a;
+	System.out.println("Division of Two number is : " + d);
 }
